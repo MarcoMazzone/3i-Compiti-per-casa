039ff729b96a7ea342c3ef34dde5c3965623b411
MarcoMazzone <marcomazz@teletu.it>
2016-11-07 09:57:16 +0100

    aggiunto es 30

---
+++ es30p69.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"dammi un numero intero n(0<= n < 1000):"<<endl;
+    cin>>n;
+    int i = 1;
+    do{
+        cout<<"la "<<i<<" esima cifra da destra è "<< n%10 <<endl;
+        n=n/10;
+        i++;
+    }while(i<=3 && n!=0);
+    return 0;
+}
